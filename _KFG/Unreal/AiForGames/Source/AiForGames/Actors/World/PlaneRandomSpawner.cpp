69b9d8425b8a424c69d1fb7197d8a1bcd4fbe0a3
Slamanderschatten <stefanpfeiffer@googlemail.com>
2026-04-16 23:53:48 +0200

    Prepare PlaneRandomSpawner

---
+++ _KFG/Unreal/AiForGames/Source/AiForGames/Actors/World/PlaneRandomSpawner.cpp
@@ -0,0 +1,38 @@
+﻿#include "PlaneRandomSpawner.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+
+APlaneRandomSpawner::APlaneRandomSpawner()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+
+void APlaneRandomSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	Spawn();
+}
+
+
+void APlaneRandomSpawner::Spawn()
+{
+	if (objectsToSpawn.Num() == 0 || spawnPlane == nullptr)
+		return;
+	
+	FVector planeScale = spawnPlane->GetActorScale3D();
+	for(FSpawnEntry entry : objectsToSpawn)
+	{
+		if (entry.actorClass == nullptr)
+			continue;
+		
+		FVector randomPosition = UKismetMathLibrary::RandomPointInBoundingBox(
+			GetActorLocation(),
+			FVector(planeScale.X/2, planeScale.Y/2, 0));
+		FActorSpawnParameters spawnParams;
+		GetWorld()->SpawnActor<AActor>(entry.actorClass, randomPosition, FRotator::ZeroRotator, spawnParams);
+	}
+	
+}
+
---
+++ _KFG/Unreal/AiForGames/Source/AiForGames/Actors/World/PlaneRandomSpawner.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Structs/FSpawnEntry.h"
+#include "PlaneRandomSpawner.generated.h"
+
+UCLASS()
+class AIFORGAMES_API APlaneRandomSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, Category="Spawning")
+	AActor* spawnPlane;
+	UPROPERTY(EditAnywhere, Category="Spawning")
+	TArray<FSpawnEntry> objectsToSpawn;
+	
+	
+	
+	
+	
+	
+	
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	
+	APlaneRandomSpawner();
+	
+	void Spawn();
+	
+};
